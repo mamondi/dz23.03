07459234d3adf5e70dcb059990b7a96e2bee85fc
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-24 13:38:28 +0200

    done

--- dz23.03/PatternMethod.cs
+++ dz23.03/PatternMethod.cs
@@ -1,1 +1,60 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+// Посередник
+public interface IMediator
+{
+    void SendMessage(Component sender, string message);
+}
+
+// Компонент
+public abstract class Component
+{
+    protected IMediator mediator;
+
+    public Component(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    public abstract void Send(string message);
+    public abstract void Receive(string message);
+}
+
+// Конкретний компонент
+public class ConcreteComponent : Component
+{
+    public ConcreteComponent(IMediator mediator) : base(mediator) { }
+
+    public override void Send(string message)
+    {
+        Console.WriteLine($"Вiдправлено повiдомлення: {message}");
+        mediator.SendMessage(this, message);
+    }
+
+    public override void Receive(string message)
+    {
+        Console.WriteLine($"Компонент отримав повiдомлення: {message}");
+    }
+}
+
+// Конкретний посередник
+public class ConcreteMediator : IMediator
+{
+    private List<Component> components = new List<Component>();
+
+    public void AddComponent(Component component)
+    {
+        components.Add(component);
+    }
+
+    public void SendMessage(Component sender, string message)
+    {
+        foreach (var component in components)
+        {
+            if (component != sender)
+                component.Receive(message);
+        }
+    }
+}
+
--- dz23.03/Program.cs
+++ dz23.03/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Створюємо посередника
+        ConcreteMediator mediator = new ConcreteMediator();
+
+        // Створюємо компоненти
+        Component component1 = new ConcreteComponent(mediator);
+        Component component2 = new ConcreteComponent(mediator);
+        Component component3 = new ConcreteComponent(mediator);
+
+        // Додаємо компоненти до посередника
+        mediator.AddComponent(component1);
+        mediator.AddComponent(component2);
+        mediator.AddComponent(component3);
+
+        // Відправляємо повідомлення
+        component1.Send("Привiт, я перший компонент!");
+    }
+}
