00d5c6406a6ec859d71790aeef97d5bb5a5d2c9f
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-24 13:17:21 +0200

    done

--- dz23.03/PatternMethod.cs
+++ dz23.03/PatternMethod.cs
@@ -1,1 +1,44 @@
-﻿
+﻿using System;
+
+public interface ICommand
+{
+    void Execute();
+}
+
+public class ConcreteCommand : ICommand
+{
+    private Receiver receiver;
+
+    public ConcreteCommand(Receiver receiver)
+    {
+        this.receiver = receiver;
+    }
+
+    public void Execute()
+    {
+        receiver.Action();
+    }
+}
+
+public class Receiver
+{
+    public void Action()
+    {
+        Console.WriteLine("Виконано дiю");
+    }
+}
+
+public class Invoker
+{
+    private ICommand command;
+
+    public void SetCommand(ICommand command)
+    {
+        this.command = command;
+    }
+
+    public void ExecuteCommand()
+    {
+        command.Execute();
+    }
+}
